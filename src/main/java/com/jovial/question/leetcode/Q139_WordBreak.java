f30330002f1069029216317e72750427640098f6
jovialcode <jovialcoding@gmail.com>
2024-05-07 12:52:23 +0900

    logic improvement

--- src/main/java/com/jovial/question/leetcode/Q139_WordBreak.java
+++ src/main/java/com/jovial/question/leetcode/Q139_WordBreak.java
@@ -5,36 +5,27 @@ import com.jovial.common.annotation.Category;
 
 import java.util.*;
 
-@Category(value = QuestionCategory.DFS)
+@Category(value = QuestionCategory.DP)
 public class Q139_WordBreak {
     public static class Solution {
         public boolean wordBreak(String s, List<String> wordDict) {
-            for(String start : wordDict){
-                StringBuilder making = new StringBuilder();
-                making.append(start);
-                //
-                dfs(s, wordDict, making);
-                if(s.equals(making.toString())) return true;
-            }
-
-            return false;
+            Set<String> wordSet = new HashSet<>(wordDict);
+            return dfs(s, wordSet, new HashMap<>());
         }
 
-        private boolean dfs(String ans, List<String> wordDict, StringBuilder making){
-            String checking = making.toString();
-            int current = making.length();
+        private boolean dfs(String s, Set<String> wordSet, Map<String, Boolean> memo) {
+            if (s.isEmpty()) return true;
+            if (memo.containsKey(s)) return memo.get(s);
 
-            if (ans.equals(checking)) return true;
-            if (ans.indexOf(checking) != 0) return false;
-
-            for (String appendWord: wordDict){
-                making.append(appendWord);
-
-                if(!dfs(ans, wordDict, making)){
-                    making.delete(current, current + appendWord.length());
-                } else return true;
+            for (int i = 1; i <= s.length(); i++) {
+                String prefix = s.substring(0, i);
+                if (wordSet.contains(prefix) && dfs(s.substring(i), wordSet, memo)) {
+                    memo.put(s, true);
+                    return true;
+                }
             }
 
+            memo.put(s, false);
             return false;
         }
     }
